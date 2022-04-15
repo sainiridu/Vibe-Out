5b1e3207e37cba8127b34cb6fdc81018025705d3
Ridham Preet <ridusaini30@gmail.com>
2022-04-15 20:32:09 +0530

    POMM Updated

--- Vibe Out/Assets/Scripts/Cube_related/DestroyScript.cs
+++ Vibe Out/Assets/Scripts/Cube_related/DestroyScript.cs
@@ -7,9 +7,19 @@ public class DestroyScript : MonoBehaviour
     public GameObject m_Blue_Particle_Prefab;
     public GameObject m_Red_Particle_Prefab;
 
-    public ScoreManager scoreManager;
+    [HideInInspector] public ScoreManager scoreManager;
 
     [HideInInspector] public int scoreToAdd;
+
+    private InstantiateObjects instantiateObjects;
+
+
+    void Start()
+    {
+        StartCoroutine(FindScoreManager());
+        instantiateObjects = FindObjectOfType<InstantiateObjects>();
+
+    }
 
 
 
@@ -19,7 +29,7 @@ public class DestroyScript : MonoBehaviour
         scoreToAdd = Random.Range(10, 25);
         if (other.gameObject.CompareTag("BlueCube"))
         {
-            GameObject blueParticle = Instantiate(m_Blue_Particle_Prefab, other.transform.position, Quaternion.identity);
+            GameObject blueParticle = Instantiate(m_Blue_Particle_Prefab, other.transform.position, Quaternion.identity, instantiateObjects.transform);
 
             Destroy(blueParticle, 2f);
 
@@ -32,7 +42,7 @@ public class DestroyScript : MonoBehaviour
         }
         else if (other.gameObject.CompareTag("RedCube"))
         {
-            GameObject redParticle = Instantiate(m_Red_Particle_Prefab, other.transform.position, Quaternion.identity);
+            GameObject redParticle = Instantiate(m_Red_Particle_Prefab, other.transform.position, Quaternion.identity, instantiateObjects.transform);
 
             Destroy(redParticle, 2f);
 
@@ -45,6 +55,14 @@ public class DestroyScript : MonoBehaviour
         }
 
 
+
+    }
+
+
+    public IEnumerator FindScoreManager()
+    {
+        yield return new WaitForSeconds(3.5f);
+        scoreManager = FindObjectOfType<ScoreManager>();
 
     }
 }
